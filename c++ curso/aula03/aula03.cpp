4faf8cfb343f98c715429a32b9f7b69d5327e5b0
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2023-07-25 09:05:54 -0300

    usando #define

---
+++ c++ curso/aula03/aula03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.14;
+#define nome cout<<"jonatas"<<endl;
+
+int main(){
+
+    cout<< pi "\n";
+    nome
+
+    system("pause");
+    return 0;
+}
