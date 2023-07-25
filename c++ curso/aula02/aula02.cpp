71d894caa0abc729b0785e6e71b594dd8883b5cf
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2023-07-25 09:05:13 -0300

    alguns tipos de variaveis

---
+++ c++ curso/aula02/aula02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    /*tipos de variaveis -> int, double, float, long, long long int, String, char, boolean...etc*/
+
+    int numero;
+    char letra;
+    double salario;
+    string nome;
+
+    cout<<"digite seu nome: "<<endl;
+    cin>>nome;
+    cout<<nome<<endl;
+
+    system("pause");
+    return 0;
+}
